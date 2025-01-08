013598c559a9e5c1de1266a59f7eeb864a5f906a
RichardPickman <drezzerock@gmail.com>
2025-01-08 15:50:40 +0100

    add pre-build to debug with HTTPS protocol

--- src/App.jsx
+++ src/App.jsx
@@ -4,61 +4,12 @@ import 'aframe-particle-system-component';
 import 'aframe-state-component';
 import 'locar';
 import { LocationBased, WebcamRenderer } from 'locar';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { BoxGeometry, MeshBasicMaterial, Scene, WebGLRenderer } from 'three';
 import './App.css';
 import './scene';
 
-const BoxAFrame = () => {
-    const box = useRef(null);
-    const [scale, setScale] = useState({ x: 1, y: 1, z: 1 });
-    const [material, setMaterial] = useState({ color: 'red' });
-    const [coordinates, setCoordinates] = useState({
-        longitude: 0,
-        latitude: 0,
-    });
-
-    useEffect(() => {
-        const updateCoordinates = (event) => {
-            console.log('event', event);
-
-            setCoordinates({ longitude, latitude });
-        };
-
-        box.current.addEventListener(
-            'gps-camera-update-position',
-            updateCoordinates,
-        );
-
-        return () => {
-            box.current.removeEventListener(
-                'gps-camera-update-position',
-                updateCoordinates,
-            );
-        };
-    }, []);
-
-    useEffect(() => {
-        console.log('scale', scale);
-        console.log('material', material);
-        console.log('coordinates', coordinates);
-
-        console.log('box', box.current);
-    }, [box]);
-
-    return (
-        <a-box
-            ref={box}
-            scale={scale}
-            material={material}
-            gps-new-entity-place={`latitude: ${coordinates.latitude}; longitude: ${coordinates.longitude}`}
-        ></a-box>
-    );
-};
-
 const AFrameScene = () => {
-    const camera = useRef(null);
-
     return (
         <a-scene
             vr-mode-ui="enabled: false"
@@ -66,18 +17,18 @@ const AFrameScene = () => {
             renderer="antialias: true; alpha: true"
         >
             <a-camera
-                ref={camera}
+                id="camera1"
+                look-controls-enabled="false"
+                arjs-device-orientation-controls="smoothingFactor: 0.1"
                 gps-new-camera="gpsMinDistance: 5"
-            ></a-camera>
+            />
 
             <a-entity
                 material="color: red"
                 geometry="primitive: box"
                 gps-new-entity-place="latitude: 50.065251; longitude: 19.951186"
                 scale="10 10 10"
-            ></a-entity>
-
-            <BoxAFrame />
+            />
         </a-scene>
     );
 };
@@ -137,8 +88,10 @@ function App() {
     }, []);
 
     return (
-        <div id="app">
+        <div>
             <AFrameScene></AFrameScene>
+
+            {renderer.current.domElement}
         </div>
     );
 }
